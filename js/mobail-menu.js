29e62808e17bbca95fa45f5cf2b2933f5b0ce53a
VladosSid <sidorsv121097@gmail.com>
2022-06-29 21:32:17 +0300

    add js modul is open secondary menu

--- js/mobail-menu.js
+++ js/mobail-menu.js
@@ -2,7 +2,8 @@
    const linckClick = document.querySelector('.nav__link--secondary');
    const openMenu = document.querySelector('.nav');
 
-   const toggleMenu = () => {
+   const toggleMenu = e => {
+      e.preventDefault();
       openMenu.classList.toggle('is-open');
    };
 
