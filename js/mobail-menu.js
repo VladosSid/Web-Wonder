31899dd61ae77cdc7b8042ccada8e850fd30e5e5
VladosSid <sidorsv121097@gmail.com>
2022-06-29 17:43:19 +0300

    refacktor js

--- js/mobail-menu.js
+++ js/mobail-menu.js
@@ -1,14 +1,3 @@
-// (() => {
-//    const linckClick = document.querySelector('.nav__link--secondary');
-//    const openMenu = document.querySelector('.nav');
-
-//    const toggleMenu = () => {
-//       openMenu.classList.toggle('is-o');
-//    };
-
-//    linckClick.addEventListener('click', toggleMenu);
-// })();
-
 (() => {
    const linckClick = document.querySelector('.nav__link--secondary');
    const openMenu = document.querySelector('.nav');
