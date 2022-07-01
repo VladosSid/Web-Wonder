4abeb0d0d8bb51608ee2cb7e389ce6b3499566f0
VladosSid <sidorsv121097@gmail.com>
2022-07-01 21:33:59 +0300

    Стили мобильного меню

--- js/mobail-menu.js
+++ js/mobail-menu.js
@@ -1,10 +1,24 @@
+// ---- открытие меню
 (() => {
-   const linckClick = document.querySelector('.nav__link--secondary');
-   const openMenu = document.querySelector('.nav');
+   const buttonMenu = document.querySelector('.header__button-mobail');
+   const openMenu = document.querySelector('.header');
 
    const toggleMenu = e => {
       e.preventDefault();
-      openMenu.classList.toggle('is-open');
+      openMenu.classList.toggle('is-hidden');
+   };
+
+   buttonMenu.addEventListener('click', toggleMenu);
+})();
+
+// ----- Вторичное меню
+(() => {
+   const linckClick = document.querySelector('.nav__link--secondary');
+   const openMenuSecondary = document.querySelector('.nav');
+
+   const toggleMenu = e => {
+      e.preventDefault();
+      openMenuSecondary.classList.toggle('is-open');
    };
 
    linckClick.addEventListener('click', toggleMenu);
