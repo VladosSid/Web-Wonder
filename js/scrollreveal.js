012b0a3ff8d81412f9c86353fd2bab41bf9f6dac
VladosSid <sidorsv121097@gmail.com>
2022-07-05 19:17:10 +0300

    add ScrollReveal

---
+++ js/scrollreveal.js
@@ -0,0 +1,12 @@
+ScrollReveal().reveal('.proses__discussion--scroll', {
+   easing: 'ease-in-out',
+   duration: 1500,
+   delay: 500,
+});
+ScrollReveal().reveal('.proses__box-positioning--scroll', {
+   origin: 'left',
+   distance: '100px',
+   easing: 'ease-in-out',
+   duration: 1500,
+   delay: 1500,
+});
