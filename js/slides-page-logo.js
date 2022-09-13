4598265d6e5004c48fa35f23790b5d3fc4eaede6
VladosSid <sidorsv121097@gmail.com>
2022-09-13 19:28:05 +0300

    add images page-logo

--- js/slides-page-logo.js
+++ js/slides-page-logo.js
@@ -1,13 +1,25 @@
-const slider = document.querySelectorAll('.slider-1');
-const slider1 = document.querySelectorAll('.slider-2');
+const slider1 = document.querySelectorAll('.slider-1');
+const slider2 = document.querySelectorAll('.slider-2');
+const slider3 = document.querySelectorAll('.slider-3');
+const slider5 = document.querySelectorAll('.slider-5');
+const slider6 = document.querySelectorAll('.slider-6');
+const slider7 = document.querySelectorAll('.slider-7');
+const slider8 = document.querySelectorAll('.slider-8');
+const slider9 = document.querySelectorAll('.slider-9');
 
 let slideIndex = 0;
 
 showSlides();
 
 function showSlides() {
-   slide(slider);
    slide(slider1);
+   slide(slider2);
+   slide(slider3);
+   slide(slider5);
+   slide(slider6);
+   slide(slider7);
+   slide(slider8);
+   slide(slider9);
 
    setTimeout(showSlides, 2000);
 }
